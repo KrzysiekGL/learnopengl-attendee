cb203b64473f3ae2d9a4308fadb6a33928a134c1
KrzysiekGL <krzysztof.gl.szczerbowski@gmail.com>
2022-06-20 19:54:30 +0200

    Add GLAD and build it with CMake

--- 01-Creating_a_window/src/main.cpp
+++ 01-Creating_a_window/src/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
-// first include glad here
-
+#include <glad/gl.h>
 #include <GLFW/glfw3.h>
 
 int main(int argc, char ** argv, char ** eval) {
 	std::cout << "01-Creating_a_window\n";
 	return 0;
 }
+
