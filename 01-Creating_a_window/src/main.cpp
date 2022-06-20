5e1940de6ed6c963075328c6b9103ad264268fad
KrzysiekGL <krzysztof.gl.szczerbowski@gmail.com>
2022-06-20 19:37:47 +0200

    Build GLFW (and add OpenGL headers) with CMake

---
+++ 01-Creating_a_window/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// first include glad here
+
+#include <GLFW/glfw3.h>
+
+int main(int argc, char ** argv, char ** eval) {
+	std::cout << "01-Creating_a_window\n";
+	return 0;
+}
