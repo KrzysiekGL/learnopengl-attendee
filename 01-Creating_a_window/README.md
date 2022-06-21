2abc451ee6c9869760649bdeacbdd6d0b8102b90
Krzysztof Szczerbowski <37380860+KrzysiekGL@users.noreply.github.com>
2022-06-21 17:08:43 +0200

    01 context creation (#1)
    * Add cmake
    
    * Add GLFW submodule
    
    * Add readme for the section
    
    * Build GLFW (and add OpenGL headers) with CMake
    
    * Add GLAD and build it with CMake
    
    * Replace building GLAD by glad_add_library with just web generated files
    
    * Create window and setup basic OpenGL & GLFW settings/callbacs
    
    * Apply PR requests

---
+++ 01-Creating_a_window/README.md
@@ -0,0 +1,3 @@
+# Getting started
+OpenGL context (window) creation with GLFW.
+Also, other libraries required to run the windowed app using OpenGL.
---
+++ 01-Creating_a_window/src/main.cpp
@@ -0,0 +1,84 @@
+/*
+ * Study of OpenGL guided by https://learnopengl.com
+ *
+ * Author: KrzysiekGL webmaster@unexpectd.com
+ * 06/2022
+ */
+
+#include <iostream>
+
+/*
+ * Be sure to include GLAD before GLFW. The include file
+ * for GLAD includes the required OpenGL headers behind
+ * the scenes (like GL/gl.h) so be sure to include GLAD
+ * before other header files that require OpenGL (like GLFW).
+ */
+#include <glad/glad.h>
+
+#include <GLFW/glfw3.h>
+
+#define WIDTH 800
+#define HEIGHT 600
+
+void framebufferSizeCallback(GLFWwindow * window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow * window) {
+	if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+int main(int argc, char ** argv, char ** eval) {
+	std::cout << "01-Creating_a_window\n";
+
+	// GLFW initialization
+	glfwInit();
+	// OpenGL version and features
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	// Window features
+	glfwWindowHint(GLFW_FLOATING, GLFW_TRUE);
+
+	// GLFW window creation
+	GLFWwindow * window = glfwCreateWindow(WIDTH, HEIGHT, "learnopengl", NULL, NULL);
+	if(window == NULL) {
+		std::cout << "Failed to create GLFW window\n";
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Map to the OpenGL function pointers with GLAD
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize OpenGL context (GLAD)\n";
+		glfwTerminate();
+		return -1;
+	}
+
+	// GLFW callbacks
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+	// OpenGL state machine settings
+	framebufferSizeCallback(window, WIDTH, HEIGHT);
+	glClearColor(.2f, .3f, .3f, 1.f);
+
+	// Game loop/Render loop
+	while(!glfwWindowShouldClose(window)) {
+		// Input
+		processInput(window);
+
+		// Rendering
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Events & Swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// Clean-up
+	glfwTerminate();
+	return 0;
+}
+
