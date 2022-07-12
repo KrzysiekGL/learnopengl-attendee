74823ed87882cbb26f775b7f372032f990d30671
KrzysiekGL <krzysztof.gl.szczerbowski@gmail.com>
2022-07-12 20:15:52 +0200

    Add loading an image and binding it to a (created) texture object

--- 04-Textures/src/main.cpp
+++ 04-Textures/src/main.cpp
@@ -11,17 +11,26 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 #include "Context.hpp"
 #include "Shader.hpp"
 
 const float vertices[] = {
-	.5, .0, .0,		1.f, 0.f, 0.f,
-	-.5,.0, .0,   0.f, 1.f, 0.f,
-	.0, .5, .0,   0.f, 0.f, 1.f,
+	.5, .0, .0,
+	-.5,.0, .0,
+	.0, .5, .0,
 };
 
 const GLuint indices[] = {
 	0, 1, 2,
+};
+
+const float texCoords[] = {
+	.0f, .0f,
+	1.f, .0f,
+	.5f, 1.f,
 };
 
 int main(int argc, char ** argv, char ** eval) {
@@ -56,11 +65,7 @@ int main(int argc, char ** argv, char ** eval) {
 
 	// Vertices
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)0);
-
-	// Colors
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)(3*sizeof(float)));
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
 
 	// Bind to default buffers (VAO (!*), VBO, EBO)
 	// *VAO keeps track of the last bound EBO while the VAO is bound.
@@ -70,8 +75,29 @@ int main(int argc, char ** argv, char ** eval) {
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 	// -----------------------------------------------------------------------------------------------
+	// 2D Texture
+	// Create a texture object
+	GLuint textureID;
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_2D, textureID);
+	// Set the texture wrapping/filtering options (on the currently bound texture object)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// Loading an image/texture
+	GLint width, height, nrChannels;
+	unsigned char * data = stbi_load("../texture/container.jpg", &width, &height, &nrChannels, 0);
+	if(data==NULL) std::cerr << "Image not loaded\n";
+	std::cout << width << " " << height << " " << nrChannels << std::endl;
+	// Generate texture (on the currently bound texture at the active texture unit)
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	// Free the image data (unsigned char *) memory
+	stbi_image_free(data);
+	// -----------------------------------------------------------------------------------------------
 	// Shader program
-	Shader basic("../shader/basic.vert", "../shader/basic.frag");
+	Shader basic("../shader/texture.vert", "../shader/texture.frag");
 	// End of temp space for rendering stuff
 	// -----------------------------------------------------------------------------------------------
 
